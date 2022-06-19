e18fb6d6bc74962aa53f86043583bf547e625a7a
dpathak96 <pathakdhrumil1796@gmail.com>
2022-06-19 13:18:56 -0700

    Print message

---
+++ print.sh
@@ -0,0 +1,15 @@
+# Printing in shell is done by echo command
+# syntax : echo INPUT MESSAGE
+
+echo world
+echo Hello World
+
+# We can also print text in colours
+# Syntax : echo -e "\e[COmMESSAGE\e[0m"
+
+## -e - To enable \e
+## \e[ - To enable colours
+## COL - colour code
+## m - End of Syntax
+## 0 - To disable colour
+
