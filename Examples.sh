c4a27f90c538f2e40f082c368142e82efe142c87
dpathak96 <pathakdhrumil1796@gmail.com>
2022-06-20 23:01:13 -0700

    read command

--- Examples.sh
+++ Examples.sh
@@ -6,3 +6,10 @@ echo Adding user ${username}
 useradd ${username}
 echo User ${username} sucessfully created
 echo Thank you
+
+
+read -p 'Enter you name: ' name
+read -p 'Enter your age: ' age
+
+echo "your Name = ${name}, Your age = $age}"
+
