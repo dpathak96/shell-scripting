637c764eab2cf68e051a1f184dce55ad94ac25e2
dpathak96 <pathakdhrumil1796@gmail.com>
2022-06-25 12:58:03 -0700

    Script for setting up components.

---
+++ Roboshop/roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+user_id=$(id -u)
+if [ "${user_id}" -ne 0 ]; then
+  echo you should be root user to perform this script
+  exit
+fi
+
+COMPONENT=$1
+if [ -z "${COMPONENT}" ]; then
+  echo Component input missing
+  exit
+fi
+
+if [ ! -e Components/${COMPONENT}.sh ]; then
+  echo Given component script doesnot exist
+  exit
+fi
+
+bash Components/${COMPONENT}.sh
