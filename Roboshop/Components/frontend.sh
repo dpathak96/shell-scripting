cb98c5f1feaee11c423f02b20d430b697e026cc6
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-03 22:16:51 -0700

    export command added

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,39 +1,27 @@
 #!bin/bash
-##echo frontend setup
-##Frontend
-##The frontend is the service in RobotShop to serve the web content over Nginx.
+LOG_FILE=/tmp/roboshop.log
+rm -rf ${LOG_FILE}
 
-##To Install Nginx.
-
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-#Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
-
-# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#Deploy in Nginx Default Location.
-
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-master static README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#Finally restart the service once to effect the changes.
-
-# systemctl restart nginx
-
-source Components/common.sh
+STAT_CHECK() {
+  if [ $1 -ne 0 ]; then
+    echo -e "\e[1;31m${2} - FAILED\e[0m"
+    exit 1
+  else
+    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+  fi
+}
 
 yum install nginx -y >>${LOG_FILE}
 STAT_CHECK $? "Nginx installation"
 
-DOWNLOAD frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
+STAT_CHECK $? "Download Nginx"
 
 rm -rf /usr/share/gninx/html/*
 STAT_CHECK $? "Remove Old HTML Pages"
 
+cd /tmp && unzip -o /tmp/frontend.zip &>>{LOG_FILE}
+STAT_CHECK $? "Extracting frontend content"
 
 cd /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
 STAT_CHECK $? "Copying frontend content"
