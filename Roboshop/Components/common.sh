90451a1abdf09e581ae23283dd6ab8c3cc97af15
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-03 21:27:51 -0700

    common script extract

---
+++ Roboshop/Components/common.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+LOG_FILE=/tmp/roboshop.log
+rm -rf ${LOG_FILE}
+
+STAT_CHECK() {
+  if [ $1 -ne 0 ]; then
+    echo -e "\e[1;31m${2} - FAILED\e[0m"
+    exit 1
+  else
+    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+  fi
+}
+
+set-hostname -skip-apply ${COMPONENT}
+
+DOWNLOAD () {
+  curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archivw/main.zip" &>>${LOG_FILE}
+  STAT_CHECK $? "Download ${1} code"
+  cd /tmp
+  unzip -o /tmp/${1}.zip &>>${LOG_FILE}
+  STAT_CHECK $? "Extract ${1} code"
+}
