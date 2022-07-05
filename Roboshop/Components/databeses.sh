5f68603e5bc08204a7959468da1a2db9cc8557cb
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-04 21:15:28 -0700

    DOWNLOAD COMMAND SETUP.

--- Roboshop/Components/databeses.sh
+++ Roboshop/Components/databeses.sh
@@ -1,15 +1,11 @@
 #!bin/bash
 
-#Install MongoDB Manual Steps.
-#Ref URL: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-red-hat/
-
-#Setup MongoDB repos.
+#-----<<<<<< MONGODB SETUP >>>>>---------
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG_FILE}
 STAT_CHECK $? "Download MongoDB Repo"
 
 yum install -y mongodb-org &>>{LOG_FILE}
 STAT_CHECK "Install MongoDB"
-
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>{LOG_FILE}
 
