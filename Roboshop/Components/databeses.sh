60d76c61686cb1e843b8e82f04aa960a5f9b0a05
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-04 21:13:07 -0700

    export command added

--- Roboshop/Components/databeses.sh
+++ Roboshop/Components/databeses.sh
@@ -1,28 +1,24 @@
 #!bin/bash
 
-Install MongoDB Manual Steps.
-Ref URL: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-red-hat/
+#Install MongoDB Manual Steps.
+#Ref URL: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-red-hat/
 
-Setup MongoDB repos.
-# curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-Install Mongo & Start Service.
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
-Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-Config file: /etc/mongod.conf
+#Setup MongoDB repos.
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG_FILE}
+STAT_CHECK $? "Download MongoDB Repo"
 
-then restart the service
+yum install -y mongodb-org &>>{LOG_FILE}
+STAT_CHECK "Install MongoDB"
 
-# systemctl restart mongod
-Every Database needs the schema to be loaded for the application to work.
-Download the schema and load it.
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>{LOG_FILE}
 
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+systemctl enable mongod &>>{LOG_FILE} && systemctl restart mongod &>>{LOG_FILE}
+STAT_CHECK $? "Start MongoDB Service"
+
+DOWNLOAD mongodb
+
+cd /tmp/mongodb.main
+mongo < catalogue.js &>>{LOG_FILE} && mongo < users.js &>>{LOG_FILE}
+STAT_CHECK $? "Load Schema"
 
