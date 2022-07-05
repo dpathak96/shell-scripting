38464a814af41e1225a06818ba9c04c9bc31c001
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-04 21:56:06 -0700

    fixed stat check script

--- Roboshop/Components/databeses.sh
+++ Roboshop/Components/databeses.sh
@@ -15,22 +15,22 @@ STAT_CHECK() {
 }
 
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG_FILE}
-STAT_CHECK $? "Download MongoDB Repo"
+STAT_CHECK  "Download MongoDB Repo"
 
 yum install -y mongodb-org &>>{LOG_FILE}
-STAT_CHECK $? "Install MongoDB"
+STAT_CHECK  "Install MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>{LOG_FILE}
 
 systemctl enable mongod &>>{LOG_FILE} && systemctl restart mongod &>>{LOG_FILE}
-STAT_CHECK $? "Start MongoDB Service"
+STAT_CHECK  "Start MongoDB Service"
 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
-STAT_CHECK $? "Download mongodb code"
+STAT_CHECK  "Download mongodb code"
 cd /tmp && unzip -o /tmp/mongodb.zip &>>${LOG_FILE}
-STAT_CHECK $? "Extract MongoDB code"
+STAT_CHECK  "Extract MongoDB code"
 
 cd /tmp/mongodb-main
 mongo < catalogue.js &>>{LOG_FILE} && mongo < users.js &>>{LOG_FILE}
-STAT_CHECK $? "Load Schema"
+STAT_CHECK  "Load Schema"
 
