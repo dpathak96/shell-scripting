b561a11badb7a73bb329cb75823459b04509a6e4
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-05 22:14:30 -0700

    changed colour of text in stat_check function

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-echo "-----<<<<<< MONGODB SETUP >>>>>---------"
+echo "-----<<<<<< "\e[1;35mMONGODB SETUP\e[0m] >>>>>---------"
 
 LOG_FILE=/tmp/roboshop.log
 rm -rf ${LOG_FILE}
 
 STAT_CHECK() {
   if [ $1 -ne 0 ]; then
-    echo -e "\e[1;31m${2} - FAILED\e[0m"
+    echo -e "\033[34m${2} - FAILED\e[0m"
     exit 1
   else
     echo -e "\e[1;33m${2} - SUCCESS\e[0m"
