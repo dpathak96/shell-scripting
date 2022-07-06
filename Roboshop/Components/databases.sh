3c044d05cfe1430678fcf5921f68cf4fdf23766d
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-05 21:45:22 -0700

    Recall Download function

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -14,6 +14,11 @@ STAT_CHECK() {
   fi
 }
 
+DOWNLOAD() {
+  curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
+  STAT_CHECK $? "Download ${1} code"
+}
+
 DOWNLOAD mongodb
 
 yum install -y mongodb-org &>>{LOG_FILE}
