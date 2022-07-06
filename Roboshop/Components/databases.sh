6521d2275ea0a67538e6d282158084c202a7150a
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-05 21:24:24 -0700

    setup download command

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -14,8 +14,7 @@ STAT_CHECK() {
   fi
 }
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>{LOG_FILE}
-STAT_CHECK $? "Download MongoDB Repo"
+DOWNLOAD mongodb
 
 yum install -y mongodb-org &>>{LOG_FILE}
 STAT_CHECK $? "Install MongoDB"
