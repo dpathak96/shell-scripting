6cea0f1c8e895aeb7e6481e17a5aed3cadf5deb8
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-07 22:08:49 -0700

    Changed curl a

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -57,9 +57,9 @@ STAT_CHECK $? "start redis"
 
 
 
-echo "----------<<<<<<<<<RABBITMQ SETUP>>>>>>>>>-------"
+echo "----------<<<<<<<<< RABBITMQ SETUP >>>>>>>>>-------"
 
-yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" -y &>>{LOG_FILE}
+yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" &{LOG_FILE}
 STAT_CHECK $? "Download RabbitMQ"
 
 
