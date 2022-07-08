daea9fa0066ec1f6c84f4323d0b21388f64868a7
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-07 22:04:17 -0700

    fixed yum command

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -59,7 +59,7 @@ STAT_CHECK $? "start redis"
 
 echo "----------<<<<<<<<<RABBITMQ SETUP>>>>>>>>>-------"
 
-yum install -Y "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" &>>{LOG_FILE}
+yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" -y &>>{LOG_FILE}
 STAT_CHECK $? "Download RabbitMQ"
 
 
