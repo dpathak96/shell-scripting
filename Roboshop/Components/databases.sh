8fc5bc1e438fd832067d5be4d82fb551abb42033
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-09 14:00:28 -0700

    RabbitMQ setup

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -62,7 +62,7 @@ echo "----------<<<<<<<<< RABBITMQ SETUP >>>>>>>>>-------"
 curl -s "https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh" | sudo bash &>>{LOG_FILE}
 STAT_CHECK $? "Repositories for rabbit mq"
 
-yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" &>>{LOG_FILE}
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>{LOG_FILE}
 STAT_CHECK $? "Install Erlang dependency"
 
 yum install rabbitmq-server -y &>>{LOG_FILE}
