6f33fd907ffce50ac70c691db2999a9579638614
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-09 13:38:04 -0700

    RabbitMQ setup

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -59,14 +59,14 @@ STAT_CHECK $? "start redis"
 
 echo "----------<<<<<<<<< RABBITMQ SETUP >>>>>>>>>-------"
 
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>{LOG_FILE}
+STAT_CHECK $? "Install Erlang dependency"
 
+curl -s "https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh" | sudo bash &>>{LOG_FILE}
+STAT_CHECK $? "Repositories for rabbit mq"
 
-
-curl -s -L -o "https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh" | sudo bash &>>{LOG_FILE}
-STAT_CHECK $? "Download Rabbitmq Repo"
-
-yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" -y &>>{LOG_FILE}
-STAT_CHECK $? "Install Erlang & RabbitMQ"
+yum install rabbitmq-server -y &>>{LOG_FILE}
+STAT_CHECK $? "Install RabbitMQ server"
 
 systemctl enable Rabbitmq-server &>>{LOG_FILE} && systemctl start rabbitmq-server &>>{LOG_FILE}
 
@@ -80,7 +80,4 @@ rabbitmqctl set_user_tags roboshop administrator &>>{LOG_FILE} && rabbitmqctl
 set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
 STAT_CHECK $? "Configure app user permission"
 
-Create application user
-# rabbitmqctl add_user roboshop roboshop123
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
