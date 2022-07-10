a98584b987c0088150211855ea4b00a0124101ba
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-09 20:12:22 -0700

    catalogue setup

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -14,13 +14,6 @@ STAT_CHECK() {
   fi
 }
 
-
-yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
-STAT_CHECK $? "Install NodeJS"
-
-useradd roboshop &>>${LOG_FILE}
-STAT_CHECK $? "Add application user"
-
 DOWNLOAD() {
   curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
   STAT_CHECK $? "Download ${1} code"
@@ -28,4 +21,8 @@ DOWNLOAD() {
 
 DOWNLOAD catalogue
 
+yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
+STAT_CHECK $? "Install NodeJS"
 
+useradd roboshop &>>${LOG_FILE}
+STAT_CHECK $? "Add application user"
