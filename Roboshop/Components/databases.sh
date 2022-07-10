7e0cc64d4f775f9bb77f9667618bbdc9b9a4059f
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-09 21:25:37 -0700

    download catalogue

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -71,10 +71,11 @@ STAT_CHECK $? "Install RabbitMQ server"
 systemctl enable rabbitmq-server &>>{LOG_FILE} && systemctl start rabbitmq-server &>>${LOG_FILE}
 STAT_CHECK $? "Start RabbitMQ"
 
-rabbitmqctl list_users | grep roboshop &>>${LOG_FILE}
+id roboshop &>>{LOG_FILE}
+
 if [ $? -ne 0 ]; then
-  rabbitmqctl add_user roboshop123 &>>${LOG_FILE}
-  STAT_CHECK $? "Create app user in Rabbitmq"
+  useradd roboshop &>>{LOG_FILE}
+  STAT_CHECK $? "Add Application user"
 fi
 
 rabbitmqctl set_user_tags roboshop administrator &>>{LOG_FILE} && rabbitmqctl
