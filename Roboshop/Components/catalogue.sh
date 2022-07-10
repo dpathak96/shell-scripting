32b4f6d9de208c545369471948dc960da710bf76
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-09 19:50:04 -0700

    catalogue download

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -1,28 +1,18 @@
-This service is responsible for showing the list of items that are to be sold by the RobotShop e-commerce portal.
+!#bin/bash
 
-This service is written in NodeJS, Hence need to install NodeJS in the system.
+source Components/common.sh
 
-# curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
-# yum install nodejs make gcc-c++ -y
-Let's now set up the catalogue application.
+yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
+STAT_CHECK $? "Install NodeJS"
 
-As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
+useradd roboshop &>>${LOG_FILE}
+STAT_CHECK $? "Add application user"
 
-So to run the catalogue service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use roboshop as the username to run the service.
+DOWNLOAD() {
+  curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
+  STAT_CHECK $? "Download ${1} code"
+}
 
-# useradd roboshop
-So let's switch to the roboshop user and run the following commands.
+DOWNLOAD catalogue
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
-NOTE: We need to update the IP address of MONGODB Server in systemd.service file
-Now, lets set up the service with systemctl.
 
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
