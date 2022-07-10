37a3ba497cdee98ededcfe458b1faec61707e100
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-10 11:54:44 -0700

    sourced common script

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -2,22 +2,7 @@
 
 echo "--------<<<<<<<catalogue setup>>>>>>>>>---------"
 
-LOG_FILE=/tmp/roboshop.log
-rm -rf ${LOG_FILE}
-
-STAT_CHECK() {
-  if [ $1 -ne 0 ]; then
-    echo -e "\e[1;31m${2} - FAILED\e[0m"
-    exit 1
-  else
-    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
-  fi
-}
-
-DOWNLOAD() {
-  curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
-  STAT_CHECK $? "Download ${1} code"
-}
+source Components/common.sh
 
 DOWNLOAD catalogue
 
