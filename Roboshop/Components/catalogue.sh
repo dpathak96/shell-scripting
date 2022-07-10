24cdac72599bc39779242806c1a6ddcbe1f4b46e
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-10 12:38:15 -0700

    setup content script

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -4,16 +4,28 @@ echo "--------<<<<<<<catalogue setup>>>>>>>>>---------"
 
 source common.sh
 
-DOWNLOAD catalogue
 
 yum install nodejs make gcc-c++ -y &>>{LOG_FILE}
 STAT_CHECK $? "Install NodeJS"
 
-useradd roboshop &>>{LOG_FILE}
+id roboshop &>>{LOG_FILE}
 
-if [ $? -ne 9 ]; then
+if [ $? -ne 0 ]; then
   useradd roboshop &>>{LOG_FILE}
   STAT_CHECK $? "Add Application user"
 fi
 
 DOWNLOAD catalogue
+
+rm -rf /home/Roboshop/catalogue && mkdir -p /home/Roboshop/catalogue && cp /tmp/catalogue-main/* /home/Roboshop/catalogue &&>>{LOG_FILE}
+STAT_CHECK $? "Copy Catalogue content"
+
+cd /home/Roboshop/catalogue && npm install &>>{LOG_FILE}
+STAT_CHECK $? "NPM install"
+
+
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
