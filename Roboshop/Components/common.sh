563d2c82125d1d34d1d05da8dc02ce875863135e
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-17 14:57:45 -0700

    fixed cd command

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -34,7 +34,7 @@ component=${1}
  fi
 
  curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>{LOG_FILE}
- STAT_CHECK $? "Download {component}"
+ STAT_CHECK $? "Download ${1}"
 
 cd /tmp
 
