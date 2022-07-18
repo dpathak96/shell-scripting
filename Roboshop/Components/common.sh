f3a1147da698b92c0a38e796cef8f58010d34c52
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-17 18:12:28 -0700

    Fix cp command

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -45,7 +45,7 @@ cd /tmp
  mkdir /home/Roboshop && mkdir /home/Roboshop/${1}
 
 
- sudo yum install npm
+ sudo yum install npm -y
  STAT_CHECK $? "NPM install"
 
  chown roboshop:roboshop -R /home/Roboshop
