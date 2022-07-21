64b5d0a54b2fa41d67fcb88ee1757e9b65d81b27
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-20 19:30:03 -0700

    updated shipping script

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -14,6 +14,17 @@ STAT_CHECK() {
 
 set-hostname -skip-apply ${COMPONENT}
 
+APP_USER_SETUP() {
+component=${1}
+ id roboshop &>>{LOG_FILE}
+
+ if [ $? -ne 0 ]; then
+  useradd roboshop &>>{LOG_FILE}
+  STAT_CHECK $? "Add Application user"
+ fi
+
+}
+
 DOWNLOAD() {
 component=${1}
   curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
@@ -29,14 +40,9 @@ component=${1}
  yum install nodejs make gcc-c++ -y &>>{LOG_FILE}
  STAT_CHECK $? "Install NodeJS"
 
- id roboshop &>>{LOG_FILE}
+APP_USER_SETUP
 
- if [ $? -ne 0 ]; then
-  useradd roboshop &>>{LOG_FILE}
-  STAT_CHECK $? "Add Application user"
- fi
-
- DOWNLOAD ${1}
+DOWNLOAD ${1}
 
  rm -rf /home/Roboshop && mkdir /home/Roboshop && mkdir /home/Roboshop/${1}
  STAT_CHECK $? "Copy ${1} Content"
