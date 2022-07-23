d54db37c74045cf4b800b5c1add1744ba52712dd
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-22 23:14:42 -0700

    updated shipping script

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -109,7 +109,7 @@ PYTHON() {
 
   DOWNLOAD ${1}
 
-  rm -rf /home/roboshop/${1} && mkdir -p /home/roboshop/${1} && cp -r /tmp/${1}-main/* /home/roboshop/${1} &>>{LOG_FILE}
+  rm -rf /home/roboshop/${1} && mkdir -p /home/Roboshop/${1} && cp -r /tmp/${1}-main/* /home/roboshop/${1} &>>{LOG_FILE}
 
   cd /home/roboshop/payment && pip3 install -r requirements.txt &>>{LOG_FILE}
   STAT_CHECK $? "Install Python dependencies"
