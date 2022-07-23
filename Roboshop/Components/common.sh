42d4ffa253f483b38d8ca1230e7a9d70ba176d1e
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-23 01:31:13 -0700

    updated shipping script

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -33,7 +33,7 @@ SYSTEMD_SETUP() {
   mv /home/Roboshop/${1}/systemd.service /etc/systemd/system/${1}.service
   STAT_CHECK $? "Moved content in system file"
 
-  systemctl daemon-reload &>>{LOG_FILE} && systemctl start ${1} &>>{LOG_FILE} && systemctl enable ${1} &>>{LOG_FILE}
+  systemctl daemon-reload &>>{LOG_FILE} && systemctl restart ${1} &>>{LOG_FILE} && systemctl enable ${1} &>>{LOG_FILE}
   STAT_CHECK $? "Start ${1} service"
 }
 
