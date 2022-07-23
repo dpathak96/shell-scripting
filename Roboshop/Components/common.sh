260c616d37151a921d4b02fa1891fb3df0e0ded2
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-22 20:55:55 -0700

    updated shipping script

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -77,7 +77,7 @@ cd /home/Roboshop/${1} && sudo yum install npm -y &>>{LOG_FILE}
 
 JAVA() {
   component=${1}
-  yum install maven -y &>>{LOG_FILE}
+  mvn install:install-file -DgroupId="javax.jms" -DartifactId="jms" -Dversion="1.1" -Dpackaging="jar" -Dfile="jms-1.1.jar" &>>{LOG_FILE}
   STAT_CHECK $? "Installing Maven"
 
  APP_USER_SETUP ${1}
