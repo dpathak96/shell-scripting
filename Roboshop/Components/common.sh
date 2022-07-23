29f4091d1703c5e27a876e71c8c9b1cbd1c06dc8
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-22 21:02:45 -0700

    updated shipping script

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -77,7 +77,7 @@ cd /home/Roboshop/${1} && sudo yum install npm -y &>>{LOG_FILE}
 
 JAVA() {
   component=${1}
-  yum install maven -e
+  yum install maven -y
   STAT_CHECK $? "Installing Maven"
 
  APP_USER_SETUP ${1}
