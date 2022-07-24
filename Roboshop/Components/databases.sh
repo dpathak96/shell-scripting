b14941170cf40b6fd94d38656710ee0f1af29fb8
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-24 15:09:16 -0700

    fix statcheck command

--- Roboshop/Components/databases.sh
+++ Roboshop/Components/databases.sh
@@ -82,7 +82,7 @@ STAT_CHECK $? "Start mysql"
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log |awk '{print $NF}')
 
 echo "ALTER USER 'root'@'localhost IDENTIFIED By 'Roboshop@1' ;" >/tmp/pass.sql
-mysql -root -p"${DEFAULT_PASSWORD}" </tmp/pass.sql &>>{LOG_FILE}
+mysql --connect-expired-password -root -p"${DEFAULT_PASSWORD}" &>>{LOG_FILE}
 STAT_CHECK $? "Setup new mysql password"
 
 
