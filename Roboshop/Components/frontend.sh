50944549b063a8cda76c54b25a17f920f6e7ba41
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-24 21:56:16 -0700

    disable sed command

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -19,11 +19,11 @@ STAT_CHECK $? "Copying frontend content"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $? "Update Nginx Config File"
 
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.interior/' \
-       -e '/cart/ s/localhost/cart.roboshop.interior/' \
-       -e '/user/ s/localhost/user.roboshop.interior/' \
-       -e '/shipping/ s/localhost/shipping.roboshop.interior/' \
-       -e '/payment/ s/localhost/payment.roboshop.interior/' /etc/nginx/default.d/roboshop.conf
+#sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.interior/' \
+#       -e '/cart/ s/localhost/cart.roboshop.interior/' \
+#       -e '/user/ s/localhost/user.roboshop.interior/' \
+#       -e '/shipping/ s/localhost/shipping.roboshop.interior/' \
+#       -e '/payment/ s/localhost/payment.roboshop.interior/' /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx && systemctl restart nginx
-STAT_CHECK $? "Restart Nginx"
+#systemctl enable nginx && systemctl restart nginx
+#STAT_CHECK $? "Restart Nginx"
