6dbf3cd9e5e2160cf7de66be9782debe102b7b08
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-24 22:45:43 -0700

    fixed mongodb error in sed command

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -5,3 +5,6 @@ source common.sh
 echo "--------<<<<<<<catalogue setup>>>>>>>>>---------"
 
 NODEJS catalogue
+
+sudo cd /tmp/catalogue-main && cp server.js /home/roboshop/catalogue/
+STAT_CHECK $? "fixed exit code error"
