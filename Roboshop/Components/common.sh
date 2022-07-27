c75dc817dceedd3689f9294d380564a2d9c367e0
dpathak96 <pathakdhrumil1796@gmail.com>
2022-07-26 18:28:11 -0700

    fixed exitcode error

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -18,7 +18,7 @@ SYSTEMD_SETUP() {
   component=${1}
   chown roboshop:roboshop -R /home/roboshop
 
-  sudo sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.interior/' \
+  sudo sed -i -e 's/MONGODB_DNSNAME/mongodb.roboshop.interior/' \
           -e 's/REDIS_ENDPOINT/redis.roboshop.interior/' \
           -e 's/MONGO_ENDPOINT/mongodb.roboshop.interior/' \
           -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.interior/' \
